5b4a06b29b51ceca5bae2a7b15f552792e579447
Hatibe Kabulantok <hatibekara@gmail.com>
2021-04-25 11:17:34 -0700

    maxChar: creates charMap

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  //every char is a prop and quantity is value
+  const charMap = {};
+
+  for (let char of str) {
+    if (charMap[char]) {
+      //if char exists in the map add 1
+      charMap[char]++;
+    } else {
+      //if char does not exist create the prop
+      charMap[char] = 1;
+    }
+  }
+}
 
 module.exports = maxChar;
