488fdcd8ec03bf8d7dc0af40b3bcc57ce04bf96b
Hatibe Kabulantok <hatibekara@gmail.com>
2021-04-26 13:50:14 -0700

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,23 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
+  //creates n rows
+  for (let row = 0; row < n; row++) {
+    //creates empty string for each row
+    let level = '';
+
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
