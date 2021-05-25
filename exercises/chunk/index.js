0ad4e91f04bc2a31afda67ff92eed59251745c82
Hatibe Kabulantok <hatibekara@gmail.com>
2021-05-25 12:58:09 -0700

    chunk: iterative solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,26 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunked = [];
+
+  for (let element of array) {
+    //last element of the new array
+    const last = chunked[chunked.length - 1];
+
+    //if there is no last element in chunked or size of the new element is equal to the given size
+    if (!last || last.length === size) {
+      //pushes a new array chuck into chunked
+      chunked.push([element]);
+    } else {
+      //chunk already exists, adds the element to it
+      last.push(element);
+    }
+  }
+  return chunked;
+}
+
+chunk([1, 2, 3, 4], 2);
+chunk([1, 2, 3, 4, 5, 6, 7, 8], 3);
 
 module.exports = chunk;
