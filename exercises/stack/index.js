2e4806af0efbf81676317699c34dad7d72226676
Hatibe Kabulantok <hatibekara@gmail.com>
2021-06-04 11:30:20 -0700

    stack: created class methods

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,16 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push() {}
+
+  pop() {}
+
+  peek() {}
+}
 
 module.exports = Stack;
