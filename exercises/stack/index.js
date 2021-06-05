d72923fc0e73e407088f828871cfcc7b1e436b0d
Hatibe Kabulantok <hatibekara@gmail.com>
2021-06-05 10:53:59 -0700

    stack: complete

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -15,11 +15,17 @@ class Stack {
     this.data = [];
   }
 
-  push() {}
+  push(record) {
+    this.data.push(record);
+  }
 
-  pop() {}
+  pop() {
+    return this.data.pop();
+  }
 
-  peek() {}
+  peek() {
+    return this.data[this.data.length - 1];
+  }
 }
 
 module.exports = Stack;
